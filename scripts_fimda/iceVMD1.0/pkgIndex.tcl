7e38f52d1fce36ba69d8385a19a98b3a4dcbdd58
Luis Rolando Guarneros Nolasco <luisguarneros@gmail.com>
2016-08-16 21:02:38 -0500

    Create pkgIndex.tcl

---
+++ scripts_fimda/iceVMD1.0/pkgIndex.tcl
@@ -0,0 +1,3 @@
+package ifneeded vmdICE 1.0 [list source [file join $dir vmdICE.tcl]]
+package ifneeded colorplot 1.1 [list source [file join $dir colorplot.tcl]]
+package ifneeded multiplot 2.0 [list source [file join $dir multiplot.tcl]]
